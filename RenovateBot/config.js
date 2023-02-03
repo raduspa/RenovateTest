56de03bcb39b2c513e268bf853adafff68b68353
Mihai Cozma <mihai.cozma@cpp.canon>
2023-02-03 15:20:49 +0200

    Test

--- RenovateBot/config.js
+++ RenovateBot/config.js
@@ -1,8 +1,9 @@
+console.log("RENOVATE_TOKEN1 ", process.env.RENOVATE_TOKEN1)
 console.log("RENOVATE_TOKEN ", process.env.RENOVATE_TOKEN)
 
 module.exports = {
   platform: 'github',
-  token: process.env.RENOVATE_TOKEN,
+  token: process.env.RENOVATE_TOKEN1,
   logLevel: 'debug',
   repositories: ['mihaicozma/RenovateTest'],
   onboarding: false,
